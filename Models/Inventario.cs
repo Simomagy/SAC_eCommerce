c16019222d3aa90ece70929a9d2d4b07023b56bd
Eco9341 <mastronardiluca7@gmail.com>
2025-01-30 10:15:04 +0100

    Creazione DaoInventario e classe Inventario

---
+++ Models/Inventario.cs
@@ -0,0 +1,12 @@
+﻿using MSSTU.DB.Utility;
+
+namespace SAC_eCommerce.Models;
+
+public class Inventario : Entity
+{
+    public int ID_Inventario { get; set; }
+    //public Prodotto Prodotto { get; set; }
+    //public Negozio Locazione { get; set; }
+    public string Tipo_Locazione { get; set; }
+    public int Quantita { get; set; }
+}
