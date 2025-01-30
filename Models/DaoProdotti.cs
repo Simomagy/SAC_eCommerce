d243bfc6451e2ee64c3773e47b447eace74e3c0e
MS Studios <simomagy02@gmail.com>
2025-01-30 10:21:09 +0100

    Ricreato DaoProdotti.cs

--- Models/DaoProdotti.cs
+++ Models/DaoProdotti.cs
@@ -1,27 +1,22 @@
 ﻿using MSSTU.DB.Utility;
-
 namespace SAC_eCommerce.Models
 {
     public class DaoProdotti
     {
         #region Inizializzazione
-
         private readonly Database _db;
         private readonly string _tabella;
-
         private DaoProdotti(IConfiguration configuration)
         {
             _db = new Database(configuration["db_name"], configuration["server_db"]);
             _tabella = configuration["tables.prodotti"];
         }
-
         #endregion
         public bool CreateRecord(Prodotto prodotto)
         {
-            
+
             var query = $"INSERT INTO {_tabella} (Codice_SKU, Nome, Descrizione, Prezzo, Categoria, Stato, Data_Inserimento) " +
                         "VALUES (@Codice_SKU, @Nome, @Descrizione, @Prezzo, @Categoria, @Stato, @Data_Inserimento)";
-
             var parameters = new Dictionary<string, object>
             {
                 {"@Codice_SKU", prodotto.Codice_SKU},
@@ -32,8 +27,8 @@ namespace SAC_eCommerce.Models
                 {"@Stato", prodotto.Stato},
                 {"@Data_Inserimento", prodotto.Data_Inserimento}
             };
-
             return _db.UpdateDb(query, parameters);
+
         }
 
         public bool UpdateRecord(Prodotto prodotto)
@@ -42,7 +37,7 @@ namespace SAC_eCommerce.Models
             var query = $"UPDATE {_tabella} SET Codice_SKU = @Codice_SKU, Nome = @Nome, Descrizione = @Descrizione, Prezzo = @Prezzo, Categoria = @Categoria, Stato = @Stato, Data_Inserimento = @Data_Inserimento WHERE ID_Prodotto = @ID_Prodotto";
             var parameters = new Dictionary<string, object>
             {
-                {"@ID_Prodotto", prodotto.ID_Prodotto},
+                {"@ID_Prodotto", prodotto.Id},
                 {"@Codice_SKU", prodotto.Codice_SKU},
                 {"@Nome", prodotto.Nome},
                 {"@Descrizione", prodotto.Descrizione},
@@ -51,61 +46,56 @@ namespace SAC_eCommerce.Models
                 {"@Stato", prodotto.Stato},
                 {"@Data_Inserimento", prodotto.Data_Inserimento}
             };
-
             return _db.UpdateDb(query, parameters);
+
         }
 
         public bool DeleteRecord(int id)
         {
-            
+
             var query = $"DELETE FROM {_tabella} WHERE ID_Prodotto = @ID_Prodotto";
             var parameters = new Dictionary<string, object>
             {
                 {"@ID_Prodotto", id}
             };
-
             return _db.UpdateDb(query, parameters);
-           
-        }
 
+        }
         public List<Prodotto> GetRecords()
         {
             var query = $"SELECT * FROM {_tabella}";
             var dataTable = _db.ReadDb(query);
-
             var prodotti = new List<Prodotto>();
-            if (dataTable == null) return prodotti;
-           
+            if (dataTable == null)
+                return prodotti;
+
             foreach (var row in dataTable)
             {
                 var prodotto = new Prodotto();
                 prodotto.TypeSort(row);
                 prodotti.Add(prodotto);
             }
-            return prodotti;        
+            return prodotti;
         }
-
         public bool ProdottoExists(int id)
-        {          
-            var query = $"SELECT COUNT(*) as Count FROM {_tabella} WHERE ID_Prodotto = @ID_Prodotto";
-            var parameters = new Dictionary<string, object>
-            {
-                {"@ID_Prodotto", id}
-            };
-            var result = _db.ReadDb(query, parameters);
-
-            return result != null && result.Count > 0 && Convert.ToInt32(result[0]["Count"]) > 0;       
-        }
-
-        public bool FindProdotto(int id)
-        {          
+        {
             var query = $"SELECT * FROM {_tabella} WHERE ID_Prodotto = @ID_Prodotto";
             var parameters = new Dictionary<string, object>
             {
                 {"@ID_Prodotto", id}
             };
             var result = _db.ReadDb(query, parameters);
-            return result != null && result.Count > 0;                
+            return result is { Count: > 0 };
+        }
+        public bool FindProdotto(int id)
+        {
+            var query = $"SELECT * FROM {_tabella} WHERE ID_Prodotto = @ID_Prodotto";
+            var parameters = new Dictionary<string, object>
+            {
+                {"@ID_Prodotto", id}
+            };
+            var result = _db.ReadDb(query, parameters);
+            return result is { Count: > 0 };
         }
     }
 }
