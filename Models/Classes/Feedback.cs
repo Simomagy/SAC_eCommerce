6786ed37725d6376a99727f2eb129c04fa34b394
LorenzoLollis <lorenzo_marino_roma@hotmail.com>
2025-02-01 14:33:33 +0100

    Implementazione dao e classe feedback

---
+++ Models/Classes/Feedback.cs
@@ -0,0 +1,15 @@
+﻿using MSSTU.DB.Utility;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography.Xml;
+
+namespace SAC_eCommerce.Models.Classes
+{
+    public class Feedback : Entity
+    {
+        public Ordine Ordine { get; set; }
+        public Utente Utente { get; set; }
+        public byte Valutazione { get; set; }
+        public string Testo { get; set; }
+        public DateTime Data_Inserimento { get; set; } = DateTime.Now;
+    }
+}
